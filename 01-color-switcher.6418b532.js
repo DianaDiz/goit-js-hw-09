5fd8978978259c19336fa690158b2e77271ca578
DianaDiz <104499575+DianaDiz@users.noreply.github.com>
2022-10-17 12:10:48 +0000

    Deploying to gh-pages from @ DianaDiz/goit-js-hw-09@4e24fd5a585cef184b6c096a3d8cabdd661d9341 🚀

---
+++ 01-color-switcher.6418b532.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled",!0);var n=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.6418b532.js.map
