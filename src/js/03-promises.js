1cadf7285d456a7a6d08a08356b4b95472467a8d
DianaDiz <dianaandpir@gmail.com>
2022-11-09 17:58:00 +0200

    додає виправлене рішення третього завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,102 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 
 const formRef = document.querySelector('.form');
-// console.log(formRef);
+
 function createPromise(position, delay) {
-  
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    obj = {position, delay};
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(obj);
+        resolve({ position, delay });
       } else {
-        reject(obj);
+        reject({ position, delay });
       }
-    }, delayValue);
-    
+    }, delay);
   })
   
 }
 
 formRef.addEventListener('submit', event => {
   event.preventDefault();
-  console.log(event);
   const {
     elements: { delay, step, amount },
   } = event.currentTarget;
-
-  const delayValue = Number(delay.value);
-  const stepValue = Number(step.value);
-  const amountValue = Number(amount.value);
-  console.log(delayValue, stepValue, amountValue);
-
-  for (let position = 1; position <= amountValue; position += 1) {
-    setTimeout(() => {
-      console.log(position, delayValue, stepValue, amountValue);
-      setTimeout(() => {
-        createPromise(position, delayValue + stepValue * position)
-          .then(res => onSuccess(res.position, res.delay))
-          .catch(error => onReject(error.position, error.delay));
-      }, delayValue);
-    }, stepValue);
-  }
+    const delayValue = Number(delay.value);
+    const stepValue = Number(step.value);
+    const amountValue = Number(amount.value);
+      for (let position = 1; position <= amountValue; position += 1) {
+        const delay = delayValue + (stepValue * (position - 1));
+        createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { position: 'center-center' });
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { position: 'center-center' });
+        });
+      }
 });
-//--------------------------------------------------------------
-
-// createPromise(position, delayValue + stepValue * position)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-
-
-
-
-
-
-//``````````````````````
-
-
-// function createPromise(position, delay) {
-  
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     obj = {position, delay};
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve(obj);
-//       } else {
-//         reject(obj);
-//       }
-//     }, delay);
-    
-//   })
-  
-// }
-
-
-// formRef.addEventListener('submit', event => {
-//   event.рreventDefault();
-//   const {
-//     elements: { delay, step, amount },
-//   } = event.currentTarget;
-//   const delayValue = Number(delay.value);
-//   const stepValue = Number(step.value);
-//   const amountValue = Number(amount.value);
-//   console.log(delayValue, stepValue, amountValue);
-
-//   for (let position = 1; position <= amountValue; position += 1) {
-//     setTimeout(() => {
-//       console.log(position, delayValue, stepValue, amountValue);
-//       setTimeout(() => {
-//         createPromise(position, delayValue + stepValue * position)
-//           .then(res => onSuccess(res.position, res.delay))
-//           .catch(error => onReject(error.position, error.delay));
-//       }, delayValue);
-//     }, stepValue)
-//   }
-// })
